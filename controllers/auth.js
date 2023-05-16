311e8f456ff03b69151b2eac38fdc63be990e05c
AymanMoka <aymanmoka94@icloud.com>
2023-05-16 13:59:36 +0300

    complete register user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 import User from "../models/user.js";
 import Http from "http-status-codes";
 import Bcrypt from "bcryptjs";
+import Jwt from "jsonwebtoken";
+import { capitalize } from "../helpers/helper.js";
 export async function register(req, res) {
   if (req.body.password !== req.body.confirmPassword) {
     return res.status(Http.BAD_REQUEST).json({
@@ -8,9 +10,10 @@ export async function register(req, res) {
       message: "Password and Confirm Password do not match",
     }); // if passowrd and confirm password do not match
   }
+  const newUsername = capitalize(req.body.username); // capitalize username
 
   const userExists = await User.findOne({
-    $or: [{ email: req.body.email }, { username: req.body.username }],
+    $or: [{ email: req.body.email.toLowerCase() }, { username: newUsername }],
   });
   if (userExists) {
     return res.status(Http.BAD_REQUEST).json({
@@ -19,7 +22,7 @@ export async function register(req, res) {
     });
   }
 
-  return Bcrypt.hashSync(req.body.password, 10, async (err, hash) => {
+  return Bcrypt.hash(req.body.password, 10, async (err, hash) => {
     if (err) {
       return res.status(Http.BAD_REQUEST).json({
         // if error while hashing
@@ -29,24 +32,32 @@ export async function register(req, res) {
 
     const newUser = new User({
       // create new user
-      username: req.body.username,
+      username: capitalize(req.body.username),
       email: req.body.email.toLowerCase(),
       password: hash,
       firstname: req.body.firstname,
       lastname: req.body.lastname,
     });
 
-    const user = await newUser.save(); // save new user
-    if (!user) {
-      return res.status(Http.BAD_REQUEST).json({
-        // if user is not saved
-        message: "Error while registering user",
+    try {
+      const user = await newUser.save(); // save user
+      const token = Jwt.sign(
+        { userId: user._id, username: user.username },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+      );
+      return res.status(Http.CREATED).json({
+        // if user created successfully
+        message: "User created successfully",
+        user: user,
+        token: token,
+      });
+    } catch (err) {
+      return res.status(Http.INTERNAL_SERVER_ERROR).json({
+        // if error while saving user
+        message: "Error while saving user",
+        error: err._message,
       });
     }
-    return res.status(Http.CREATED).json({
-      // if user is saved
-      message: "User registered successfully",
-      data: user,
-    });
   });
 }
---
+++ helpers/helper.js
@@ -0,0 +1,4 @@
+export function capitalize(name) {
+   const newName = name.charAt(0).toUpperCase() 
+    return newName + name.slice(1);
+}
